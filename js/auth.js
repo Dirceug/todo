1fcc611c9310617c3e1030f56d64684b66926831
Dirceu Garcia <dirceu.garcia@gmail.com>
2025-02-12 23:23:03 -0300

    18, 19, 20, 21, 22, 23 e 24 autenticação por redes sociais e deletar usuário

--- js/auth.js
+++ js/auth.js
@@ -1,3 +1,6 @@
+//Traduz para portuguêsBR a autenticação do Firebase
+firebase.auth().languageCode = "pt-BR"
+
 //Função que centraliza e trata a autenticação
 firebase.auth().onAuthStateChanged(function (user){
     hideItem(loading)
@@ -35,11 +38,11 @@ function signOut(){
     })
 }
 
-//função que permite o usuário fazer a verificação do e-maildele
+//função que permite o usuário fazer a verificação do e-mail dele
 function sendEmailVerification(){
     showItem(loading)
     var user = firebase.auth().currentUser
-    user.sendEmailVerification().then(function (){
+    user.sendEmailVerification(actionCodeSettings).then(function (){
         alert("E-mail de verificação foi enviado para: " + user.email + "! Verifique a sua caixa de entrada,")
     }).catch(function(error){
         alert("Houve um erro ao enviar o e-mail de verificação")
@@ -49,4 +52,92 @@ function sendEmailVerification(){
     })
 }
 
+//função que permite o usuário redefinir a senha dele
+function sendPasswordResetEmail(){
+    var email = prompt("Redefinir senha! Informe o seu endereço de e-mail.", authForm.email.value)
+    if(email){
+        showItem(loading)
+        firebase.auth().sendPasswordResetEmail(email, actionCodeSettings).then(function(){
+            alert("E-mail de redefinição de senha foi enviado para: " + email + "!")
+        }).catch(function (error){
+            alert("Houve um erro ao enviar e-mail de redefinição de senha!")
+            console.log(error)
+        }).finally(function(){
+            hideItem(loading)
+        }) 
+    }else {
+        alert("É pŕeciso preencher o campo de e-mail para redefinir a sneha")
+    }
+}
+
+//Função que permite a autenticação pelo Google:
+function signInWithGoogle(){
+    showItem(loading)
+    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider()).catch(function (error){
+        alert("Houve um erro ao autenticar usando o Google")
+        console.log(error)
+        hideItem(loading)
+    })
+}
+
+//Função que permite a autenticação pelo Github:
+function signInWithGithub(){
+    showItem(loading)
+    firebase.auth().signInWithPopup(new firebase.auth.GithubAuthProvider()).catch(function (error){
+        alert("Houve um erro ao autenticar usando o Github")
+        console.log(error)
+        hideItem(loading)
+    })
+}
+
+//Função que permite a autenticação pelo Facebook:
+function signInWithFacebook(){
+    showItem(loading)
+    firebase.auth().signInWithPopup(new firebase.auth.FacebookAuthProvider()).catch(function (error){
+        alert("Houve um erro ao autenticar usando o Facebook")
+        console.log(error)
+        hideItem(loading)
+    })
+}
+
+//função que permite atulizar nomes de usuários
+function updateUserName(){
+    var newUserName = prompt("Informe um novo nome de usuário: ", userName.innerHTML)
+    if(newUserName && newUserName != "") {
+        var user = firebase.auth().currentUser
+        userName.innerHTML = newUserName
+        showItem(loading)
+        firebase.auth().currentUser.updateProfile({
+            displayName:newUserName
+        }).catch(function(error){
+            alert("Houve um erro ao alterar o nome do usuário")
+            console.log(error)
+            hideItem(loading)
+        }).finally(function(){
+            hideItem(loading)
+        })
+    } else {
+        alert("O nome do usuário não pode ser em branco")
+    }
+}
+
+//função que permite remover contas de usuário
+
+function deleteUserAccount(){
+    var confirmation = confirm("Realmente deseja excluir a sua conta?")
+    if (confirmation){
+        showItem(loading)
+        //firebase.auth().currentUser.deleteUserAccount
+        firebase.auth().currentUser.delete().then(function (){
+            alert("Conta removida com sucesso")
+        }).catch(function (error){
+            alert("Houve um erro ao remover a sua conta!")
+            console.log(error)
+        }).finally(function (){
+            hideItem(loading)
+        })
+    }
+}
+
 //firebase.auth().signOut().then(function (){alert("Usuário saiu")})
+
--- js/utils.js
+++ js/utils.js
@@ -13,21 +13,28 @@ var userEmail = document.getElementById('userEmail')
 var sendEmailVerificationDiv = document.getElementById('sendEmailVerificationDiv')
 var emailVerified = document.getElementById('emailVerified')
 
+var passwordReset = document.getElementById('passwordReset')
+
+var userImg = document.getElementById('userImg')
+var userName = document.getElementById('userName')
+
 
 // Alterar o formulário de autenticação para o cadastro de novas contas
 function toggleToRegister() {
   authForm.submitAuthForm.innerHTML = 'Cadastrar conta'
   authFormTitle.innerHTML = 'Insira seus dados para se cadastrar'
-  hideItem(register)
-  showItem(access)
+  hideItem(register)//esconder atalhao para registro
+  hideItem(passwordReset)//Esconder opção de redefinição se senha
+  showItem(access)//mostrar atalho para acesso
 }
 
 // Alterar o formulário de autenticação para o acesso de contas já existentes
 function toggleToAccess() {
   authForm.submitAuthForm.innerHTML = 'Acessar'
   authFormTitle.innerHTML = 'Acesse a sua conta para continuar'
-  hideItem(access)
-  showItem(register)
+  hideItem(access)//esconder atalho para acesso
+  showItem(register)//mostrar atalhao para acessar conta
+  showItem(passwordReset)//mostrar opção de redefinição se senha
 }
 
 // Simpplifica a exibição de elementos da página
@@ -43,13 +50,22 @@ function hideItem(element) {
 //Mostrar conteúdo para usuários autenticados
 function showUserContent(user){
   console.log(user)
-  if(user.emailVerified){
-    emailVerified.innerHTML = "E-mail verificado"
+  if(user.providerData[0].providerId != 'password'){
+    emailVerified.innerHTML = "Autenticação por provedor confiável, não é necessário verificar e-mail"
     hideItem(sendEmailVerificationDiv)
   } else {
-    emailVerified.innerHTML = "E-mail não verificado"
-    showItem(sendEmailVerificationDiv)
+    if(user.emailVerified){
+      emailVerified.innerHTML = "E-mail verificado!"
+      hideItem(sendEmailVerificationDiv)
+    } else {
+      emailVerified.innerHTML = "E-mail não verificado!"
+      showItem(sendEmailVerificationDiv)
+    }
   }
+  userImg.src = user.photoURL? user.photoURL : "img/unknownUser.png"
+  userName.innerHTML = user.displayName
+
+
   userEmail.innerHTML = user.email
   hideItem(auth)
   showItem(userContent)
@@ -60,4 +76,9 @@ function showAuth(){
   authForm.password.value = ""
   hideItem(userContent)
   showItem(auth)
+}
+
+//Atributos extras de configuração de e-mail
+var actionCodeSettings = {
+  url: "http://127.0.0.1:5500/"
 }
